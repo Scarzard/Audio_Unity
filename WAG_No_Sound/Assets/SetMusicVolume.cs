75c6c25ded8588a456365994bbee33e0c6be7560
Lidux <36154195+JoanBarduena@users.noreply.github.com>
2020-05-15 12:18:01 +0200

    Exercice5

---
+++ WAG_No_Sound/Assets/SetMusicVolume.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+
+public class SetMusicVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
